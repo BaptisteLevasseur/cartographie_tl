aeb99a42afd02cb6bfee357366f8c623b5edb1ff
Baptiste <baptiste.levasseur@supelec.fr>
2016-02-28 19:03:30 +0100

    Modification controller

--- scripts/controler.py
+++ scripts/controler.py
@@ -6,33 +6,34 @@ import geometry_msgs.msg
 import std_msgs.msg
 
 
+L=33*10**(-2)
+R=20*10**(-2)
+wl=0
+wr=0
+leftWheelCommand=rospy.Publisher('vrep/leftWheelCommand',std_msgs.msg.Float64, queue_size=1)
+rightWheelCommand=rospy.Publisher('vrep/rightWheelCommand',std_msgs.msg.Float64, queue_size=1)
+
 def convert(msg):
-    leftWheelCommand=rospy.Publisher('vrep/leftWheelCommand',std_msgs.msg.Float64, queue_size=1)
-    rightWheelCommand=rospy.Publisher('vrep/rightWheelCommand',std_msgs.msg.Float64, queue_size=1)
-    rate=rospy.Rate(1)
-    L=33*10**(-2)
-    R=20*10**(-2)
     v=msg.linear.x
     w=msg.angular.z
+    global wl
+    global wr
     wl=(v-w*L/2)/R
     wr=(v+w*L/2)/R
     rospy.loginfo("I heard %s", wl)
     rospy.loginfo("I heard then %s", wr)
 
+
     leftWheelCommand.publish(std_msgs.msg.Float64(wl))
     rightWheelCommand.publish(std_msgs.msg.Float64(wr))
-
-    rate.sleep()
-
-    leftWheelCommand.publish(std_msgs.msg.Float64(0))
-    rightWheelCommand.publish(std_msgs.msg.Float64(0))
-
 
 def controler():
     rospy.init_node('controler', anonymous=True)
     hellow_world="hellow_world"
     rospy.loginfo(hellow_world)
+    rate=rospy.Rate(1)
     rospy.Subscriber('cmd_vel', geometry_msgs.msg.Twist, convert)
+
     rospy.spin()
 
 
--- scripts/controler2.py
+++ scripts/controler2.py
@@ -2,17 +2,18 @@
 
 
 import rospy
-import cartographie_tl.msg
+import geometry_msgs.msg
 import std_msgs.msg
 
 
 def convert(msg):
     leftWheelCommand=rospy.Publisher('vrep/leftWheelCommand',std_msgs.msg.Float64, queue_size=1)
     rightWheelCommand=rospy.Publisher('vrep/rightWheelCommand',std_msgs.msg.Float64, queue_size=1)
+    rate=rospy.Rate(1)
     L=33*10**(-2)
     R=20*10**(-2)
-    w=msg.w
-    v=msg.v
+    v=msg.linear.x
+    w=msg.angular.z
     wl=(v-w*L/2)/R
     wr=(v+w*L/2)/R
     rospy.loginfo("I heard %s", wl)
@@ -21,11 +22,17 @@ def convert(msg):
     leftWheelCommand.publish(std_msgs.msg.Float64(wl))
     rightWheelCommand.publish(std_msgs.msg.Float64(wr))
 
+    rate.sleep()
+
+    leftWheelCommand.publish(std_msgs.msg.Float64(0))
+    rightWheelCommand.publish(std_msgs.msg.Float64(0))
+
+
 def controler():
     rospy.init_node('controler', anonymous=True)
     hellow_world="hellow_world"
     rospy.loginfo(hellow_world)
-    rospy.Subscriber('wheelCommand', cartographie_tl.msg.WheelCommand, convert)
+    rospy.Subscriber('cmd_vel', geometry_msgs.msg.Twist, convert)
     rospy.spin()
 
 
@@ -34,4 +41,3 @@ if __name__=='__main__':
     controler()
 
 
-    dsllsklk
